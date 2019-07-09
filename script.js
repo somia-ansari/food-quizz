39e6c39cb8a79f323bdc3a1c806327ccc88fe467
Somia Sohail Ansari <48806685+somia-ansari@users.noreply.github.com>
2019-07-09 14:04:15 +0500

    shorten JS efficiently

--- script.js
+++ script.js
@@ -1,69 +1,22 @@
-let score = 0;
-
-function teaAnswer() {
-    let submitted = document.querySelector("#tea");
-    submitted.classList.add('done');
-    let correct = document.querySelector('input#leaves').checked;
-    if (correct == true) {
-        score += 1;
-    }
-}
-
-function sugarAnswer() {
-    let submitted = document.querySelector("#sugar");
-    submitted.classList.add('done');
-    let correct = document.querySelector('input#sugarcane').checked;
-    if (correct == true) {
-        score += 1;
-    }
-}
-
-function rotiAnswer() {
-    let submitted = document.querySelector("#roti");
-    submitted.classList.add('done');
-    let correct = document.querySelector('input#flour-salt-water').checked;
-    if (correct == true) {
-        score += 1;
-    }
-}
-
-function milkAnswer() {
-    let submitted = document.querySelector("#youghurt-made");
-    submitted.classList.add('done');
-    let correct = document.querySelector('input#milk').checked;
-    if (correct == true) {
-        score += 1;
-    }
-}
-
-function edibleAnswer() {
-    let submitted = document.querySelector("#edible-food");
-    submitted.classList.add('done');
-    let correct = document.querySelector('input#edible').checked;
-    if (correct == true) {
-        score += 1;
-    }
-}
-
-function honeyAnswer() {
-    let submitted = document.querySelector("#honey-rot");
-    submitted.classList.add('done');
-    let correct = document.querySelector('input#honey').checked;
-    if (correct == true) {
-        score += 1;
-    }
-}
-
-function gelatinAnswer() {
-    let submitted = document.querySelector("#gelatin");
-    submitted.classList.add('done');
-    let correct = document.querySelector('input#skin-and-bones').checked;
-    if (correct == true) {
-        score += 1;
-    }
-}
-
 function checkScores() {
+
+    let score = 0;
+    // answers
+    let firstAnswer = document.querySelector('input#leaves').checked;
+    let secondAnswer = document.querySelector('input#sugarcane').checked;
+    let thirdAnswer = document.querySelector('input#flour-salt-water').checked;
+    let forthAnswer = document.querySelector('input#milk').checked;
+    let fifthAnswer = document.querySelector('input#edible').checked;
+    let sixthAnswer = document.querySelector('input#honey').checked;
+    let seventhAnswer = document.querySelector('input#skin-and-bones').checked;
+
+    let answers = [firstAnswer, secondAnswer, thirdAnswer, forthAnswer, fifthAnswer, sixthAnswer, seventhAnswer]
+    for (let i = 0; i < answers.length; i++) {
+
+        if (answers[i] == true) {
+            score += 1;
+        }
+    }
     if (score == 7) {
         alert("Congratulations!! You did a great job. You have scored " + score + " out of 7");
 
